aaa88c54e33f68868cd8357cfd0a2cf6b32b62e4
kamshir <iammaksim2000shirshikov@gmail.com>
2023-08-10 09:20:03 +0300

    cart

---
+++ js/cart.js
@@ -0,0 +1,19 @@
+const cart = () => {
+  const
+    cartBtn       = document.getElementById('cart'),
+    cartModal     = document.querySelector('.cart'),
+    cartCloseBtn  = cartModal.querySelector('.cart-close')
+
+  const openCart = () => {
+    cartModal.style.display = 'flex'
+  }
+
+  const closeCart = () => {
+    cartModal.style.display = ''
+  }
+
+  cartBtn.addEventListener('click', openCart)
+  cartCloseBtn.addEventListener('click', closeCart)
+}
+
+cart()
