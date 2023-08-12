f1cdb60a94edcc09041bfcf76c544b3f20a47429
kamshir <iammaksim2000shirshikov@gmail.com>
2023-08-12 13:58:06 +0300

    load

--- src/modules/load.js
+++ src/modules/load.js
@@ -1,8 +1,8 @@
-const getData = () => {
+const load = () => {
   return fetch('https://o-zone-42e77-default-rtdb.firebaseio.com/goods.json')
       .then((response) => {
         return response.json()
       })
 }
 
-export default getData
+export default load
--- src/modules/second.js
+++ src/modules/second.js
@@ -1,17 +1,12 @@
-import getData from "./getData";
+import load from "./load";
 import postData from "./postData";
+import renderGoods from "./renderGoods";
 
 const second = () => {
   const cartBtn = document.getElementById('cart')
 
-  cartBtn.addEventListener('click', () => {
-    postData().then((data) => {
-      console.log(data);
-
-      getData().then((data) => {
-        console.log(data);
-      })
-    })
+  load().then((data) => {
+    renderGoods(data)
   })
 }
 
